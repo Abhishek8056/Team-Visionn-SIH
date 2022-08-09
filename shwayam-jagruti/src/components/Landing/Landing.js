1c392fef28c6ab3a5ba38af1e3879bf777f80f03
Ravindra Kumar <ravindrakar9658@gmail.com>
2022-08-09 17:59:25 +0530

    forntpage lagbag ho gaya

--- shwayam-jagruti/src/components/Landing/Landing.js
+++ shwayam-jagruti/src/components/Landing/Landing.js
@@ -12,7 +12,7 @@ const Landing = () => {
 
                         <h1 className='heading'>which Government </ h1>
 
-                        <h1 className='heading'>Scheme is </ h1>
+                        <h1 className='heading'> Scheme is </ h1>
                         <h1 className='heading'>benificial for you.</ h1>
                         <br />
                         <br />
@@ -29,8 +29,8 @@ const Landing = () => {
                     </div>
                     <div class=" scheme-box-row row">
                         <div class="col-sm">
-                            
-                          
+
+
                         </div>
                         <div class="scheme-box-img col-sm">
                             <img src="img/notepad.svg" alt="" />
@@ -43,7 +43,30 @@ const Landing = () => {
                 </div>
                 <div className="top-scheme">
                     <div className="top-scheme-heading">
-                     <h1>Top Government Scheme</h1>   
+                        <h1>Top Government Scheme</h1>
+                    </div>
+                    <div class=" top-scheme-row row">
+                        <div class=" card-top col-sm">
+                            <div class="card-body">
+                                <h5 class="card-title">Pradhan Mantri Annadata Aay SanraksHan Abhiyan (PM AASHA)</h5>
+                                <p class="card-text">Pradhan Mantri Annadata Aay SanraksHan Abhiyan (PM AASHA) is a scheme that aims at ensuring fair price for farmers and their produce.
+
+                                    By strengthening the procurement process, the PM-AASHA scheme will improve the income of the farmers to a greater extent..</p>
+                            </div>
+                        </div>
+                        <div class=" card-top col-sm">
+                            <div class="card-body">
+                                <h5 class="card-title">Pradhan Mantri Awaas Yojana (Gramin) </h5>
+                                <p class="card-text">To provide pucca house to all who are houseless and living in dilapidated houses in rural areas. The overall target is to construct 2.95 crore pucca houses with basic amenities by March, 2024.</p>
+                            </div>
+                        </div>
+                        <div class=" card-top col-sm">
+                            <div class="card-body">
+                                <h5 class="card-title">National Rurban Mission (NRuM) Vision</h5>
+                                <p class="card-text">The Mission aims at development of 300 Rurban clusters, in the next five years. These clusters would be strengthened with the required amenities, for which it is proposed that resources be mobilized through convergence of various schemes of the Government, over and above which a Critical Gap Funding (CGF) would be provided under this Mission, for focused development of these clusters.</p>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>

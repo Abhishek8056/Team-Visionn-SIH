f1367996cb2be700a1421ca044b53f360e3c861f
Ravindra Kumar <ravindrakar9658@gmail.com>
2022-08-11 00:19:55 +0530

    login page added in the app.js

--- shwayam-jagruti/src/App.js
+++ shwayam-jagruti/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css'; 
 import Navbar from './components/Navbar/Navbar';
 import Landing from './components/Landing/Landing';
-
+import Login2 from './components/main_login_page/Login2';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Landing/>
+      <Login2/>
+
     </>
   );
 }

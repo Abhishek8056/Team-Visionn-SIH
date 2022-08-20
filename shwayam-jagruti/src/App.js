afb698a103bda8781815efd2aa4431ef31607e23
abhhishek singh <singhabhi1746@gmail.com>
2022-08-20 14:03:54 +0530

    error

--- shwayam-jagruti/src/App.js
+++ shwayam-jagruti/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Router, Route, Switch } from "react-router";
+//import { createBrowserHistory } from "history";
 import './App.css'; 
 import Navbar from './Components/Navbar/Navbar.js';
 import Landing from './Components/Landing/Landing.js'
+import { Redirect } from "react-router-dom";
 
 import Signup from "./Components/Register_login_page/Signup";
 import Login from "./Components/Register_login_page/Login";
@@ -17,6 +20,25 @@ function App() {
     <div className="App">
         <Navbar/>
         <Landing/>
+        <Router history={history}>
+          <Switch>
+            <PrivateRoute path="/Dashboard/UserDetails" component={UserDetails} />
+            <PublicRoute
+              restricted={true}
+              component={Signup}
+              path="/Signup"
+              exact
+            />
+            <PublicRoute restricted={true} component={Login} path="/" exact />
+            <PrivateRoute
+              component={ChangePassword}
+              path="/Dashboard/ChangePassword"
+              exact
+            />
+            <PrivateRoute component={Dashboard} path="/Dashboard" exact />
+            <Route render={() => <Redirect to={{ pathname: "/" }} />} />
+          </Switch>
+        </Router>
     </div>
   );
 }

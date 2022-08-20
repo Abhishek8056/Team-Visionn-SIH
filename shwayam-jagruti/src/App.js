35803c2a43064c8385eef2f291200d14e6b10580
abhhishek singh <singhabhi1746@gmail.com>
2022-08-20 13:35:17 +0530

    imports update

--- shwayam-jagruti/src/App.js
+++ shwayam-jagruti/src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar.js';
 import Landing from './Components/Landing/Landing.js'
 
+import Signup from "./Components/Register_login_page/Signup";
+import Login from "./Components/Register_login_page/Login";
+import Dashboard from "./Components/Register_login_page/Dashboard";
+import PrivateRoute from "./Components/Register_login_page/PrivateRoute";
+import PublicRoute from "./Components/Register_login_page/PublicRoute";
+import ChangePassword from "./Components/Register_login_page/ChangePassword";
+import UserDetails from "./Components/Register_login_page/UserDetails";
+
 function App() {
   return (
   
